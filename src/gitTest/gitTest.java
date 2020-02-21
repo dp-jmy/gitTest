cfe9b939a639b4a69db240e6cd0b6d1c183e097a
jmy <thou5607@gamil.com>
2020-02-21 12:54:26 +0900

    머지후푸

--- src/gitTest/gitTest.java
+++ src/gitTest/gitTest.java
@@ -4,6 +4,6 @@ public class gitTest {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요4444dfdfdf");
 		System.out.println("sdfsdf");
-		System.out.println("허브에서 수정")
+		System.out.println("허브에서 수정");
 	}
 }
