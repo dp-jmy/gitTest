14db22f236170004b2ef4f549d6a40cb9b63a8ea
jmy <thou5607@gamil.com>
2020-02-21 12:43:30 +0900

    다섯번째 이클립스

--- src/gitTest/gitTest.java
+++ src/gitTest/gitTest.java
@@ -2,7 +2,7 @@ package gitTest;
 
 public class gitTest {
 	public static void main(String[] args) {
-		System.out.println("안녕하세요");
+		System.out.println("안녕하세요4444");
 		System.out.println("sdfsdf");
 	}
 }
